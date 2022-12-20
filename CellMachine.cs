b477705529113bfe4711e6c4cd1eacf07ce904fd
Илья Троненко <BigWander@mail.ru>
2022-12-20 20:17:34 +0700

    Добавьте файлы проекта.

---
+++ CellMachine.cs
@@ -0,0 +1,62 @@
+﻿using DeleteConsole;
+using System.Text.RegularExpressions;
+
+namespace CA_3_Final
+{
+    internal class CellMachine
+    {
+        public int Cycle(string info)
+        {
+            int result;
+            if (int.TryParse(info, out result))
+            {
+                return result;
+            }
+
+            if (info.Contains("not"))
+            {
+                return Cycle(info.Replace("not", "1xor"));
+            }
+
+            if (info.Contains('('))
+            {
+                var lastOpenIndex = -1;
+                int firstClosed = -1;
+                for (int i = 0; i < info.Length; i++)
+                {
+                    if (info[i] == '(')
+                    {
+                        lastOpenIndex = i;
+                    }
+                    else if (info[i] == ')')
+                    {
+                        firstClosed = i;
+                        break;
+                    }
+                }
+
+                var test1 = info.Substring(lastOpenIndex, firstClosed - lastOpenIndex + 1);
+                var test2 = info.Substring(lastOpenIndex + 1, firstClosed - lastOpenIndex - 1);
+
+                return Cycle(info.Replace(test1, Cycle(test2).ToString()));
+            }
+
+            foreach (var operation in Operations.LogicalOperations.Keys)
+            {
+                if (info.Contains(operation))
+                {
+                    Regex regex = new Regex($@"\d+{operation}\d+");
+
+                    foreach (Match match in regex.Matches(info))
+                    {
+                        info = info.Replace(match.Value, new Calculator(new Parser(match.Value)).ResultLine.ToString());
+                    }
+                    
+                    return Cycle(info);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using CA_3_Final;
+
+Console.WriteLine("Введите логическое выражение: ");
+string logicalInput = Console.ReadLine();
+
+Console.WriteLine(new CellMachine().Cycle(logicalInput));
+
+
+Console.WriteLine("Введите числа для расчета суммы: ");
+string input = Console.ReadLine();
+
+var numbers = input.Split("+").Select(number => number.ToEightBitBinary()).ToArray();
+
+var summator = new Summator(numbers);
+summator.Work();
+
+Console.WriteLine(summator.Result);
